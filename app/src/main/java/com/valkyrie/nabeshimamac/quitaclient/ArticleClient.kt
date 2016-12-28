3bedd35e5845e82f1635e64fd7618cb174db3ab8
YukiNabeshima <yukinabe3561@icloud.com>
2016-12-28 16:43:12 +0900

    Day2 最終分

---
+++ app/src/main/java/com/valkyrie/nabeshimamac/quitaclient/ArticleClient.kt
@@ -0,0 +1,12 @@
+package com.valkyrie.nabeshimamac.quitaclient
+
+import retrofit2.http.GET
+import retrofit2.http.Query
+import rx.Observable
+
+interface ArticleClient {
+
+    @GET("/api/v2/items")
+    fun search(@Query("query") query: String): Observable<List<Article>>
+
+}
--- app/src/main/java/com/valkyrie/nabeshimamac/quitaclient/MainActivity.kt
+++ app/src/main/java/com/valkyrie/nabeshimamac/quitaclient/MainActivity.kt
@@ -2,7 +2,15 @@ package com.valkyrie.nabeshimamac.quitaclient
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Button
+import android.widget.EditText
 import android.widget.ListView
+import com.google.gson.FieldNamingPolicy
+import com.google.gson.GsonBuilder
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import rx.schedulers.Schedulers
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,14 +19,36 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val listAdapter = ArticleListAdapter(applicationContext)
-        listAdapter.articles = listOf(dummyArticle("Kotlin入門","たろう"), dummyArticle("Java入門", "じろう"))
-        val listView: ListView = findViewById(R.id.list_view)as ListView
+        listAdapter.articles = listOf(dummyArticle("Kotlin入門", "たろう"), dummyArticle("Java入門", "じろう"))
+        val listView: ListView = findViewById(R.id.list_view) as ListView
         listView.adapter = listAdapter
         listView.setOnItemClickListener { adapterView, view, position, id ->
             val article = listAdapter.articles[position]
-            ArticleActivity.intent(this, article).let {startActivity(it)
+            ArticleActivity.intent(this, article).let {
+                startActivity(it)
             }
         }
+
+        val gson = GsonBuilder()
+                .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
+                .create()
+        val retrofit = Retrofit.Builder()
+                .baseUrl("https://qiita.com")
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+                .build()
+        val articleClient = retrofit.create(ArticleClient::class.java)
+
+        val queryEditText = findViewById(R.id.query_edit_text) as EditText
+
+        val searchButton = findViewById(R.id.search_button) as Button
+
+        searchButton.setOnClickListener {
+            articleClient.search(queryEditText.text.toString())
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidS)
+        }
+
 
     }
 
